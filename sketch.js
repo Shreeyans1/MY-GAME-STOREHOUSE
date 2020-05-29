fdf12192af3f894ffde9a914f71e20e48406c67c
Shreeyans Sharma <64155592+Shreeyans1@users.noreply.github.com>
2020-05-29 11:01:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,120 +1,33 @@
-var trex, trex_running, trex_collided;
-var ground, invisibleGround, groundImage;
-
-var cloudsGroup, cloudImage;
-var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
-
-var score;
-
-
-function preload(){
-  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
-  trex_collided = loadImage("trex_collided.png");
-  
-  groundImage = loadImage("ground2.png");
-  
-  cloudImage = loadImage("cloud.png");
-  
-  obstacle1 = loadImage("obstacle1.png");
-  obstacle2 = loadImage("obstacle2.png");
-  obstacle3 = loadImage("obstacle3.png");
-  obstacle4 = loadImage("obstacle4.png");
-  obstacle5 = loadImage("obstacle5.png");
-  obstacle6 = loadImage("obstacle6.png");
-}
-
+var car,wall;
+var speed,weight;
 function setup() {
-  createCanvas(600, 200);
-  
-  trex = createSprite(50,180,20,50);
-  trex.addAnimation("running", trex_running);
-  trex.scale = 0.5;
-  
-  ground = createSprite(200,180,400,20);
-  ground.addImage("ground",groundImage);
-  ground.x = ground.width /2;
-  ground.velocityX = -4;
-  
-  invisibleGround = createSprite(200,190,400,10);
-  invisibleGround.visible = false;
-  
-  cloudsGroup = new Group();
-  obstaclesGroup = new Group();
-  
-  score = 0;
+  createCanvas(1600,400);
+  speed = random(55,90);
+  weight = random(400,1500);
+  wall = createSprite(1500,200,60,height/2);
+  wall.shapeColor = "white";
+  car = createSprite(50,200,50,50);
+  car.velocityX = speed;
+  car.shapeColor = "white";
 }
 
 function draw() {
-  background(180);
-  
-  score = score + Math.round(getFrameRate()/60);
-  text("Score: "+ score, 500,50);
-  
-  if(keyDown("space")) {
-    trex.velocityY = -10;
+  background(0);  
+  if(car.x-wall.x<wall.width/2+car.width/2
+    &&wall.x-car.x<wall.width/2+car.width/2
+    &&car.y-wall.y<wall.height/2+car.height/2
+    &&wall.y-car.y<wall.height/2+car.height/2){
+  car.velocityX = 0;
+  var deformation = 0.5*weight*speed*speed/22500;
   }
-  
-  trex.velocityY = trex.velocityY + 0.8
-  
-  if (ground.x < 0){
-    ground.x = ground.width/2;
+  if (deformation<100){
+    car.shapeColor = color(0,255,0);
   }
-  
-  trex.collide(invisibleGround);
-  spawnClouds();
-  spawnObstacles();
+  if (deformation>100&&deformation<180){
+    car.shapeColor = color(230,230,0);
+  }
+  if (deformation>180){
+    car.shapeColor = color(255,0,0);
+  }
   drawSprites();
-}
-
-function spawnClouds() {
-  //write code here to spawn the clouds
-  if (frameCount % 60 === 0) {
-    var cloud = createSprite(600,120,40,10);
-    cloud.y = Math.round(random(80,120));
-    cloud.addImage(cloudImage);
-    cloud.scale = 0.5;
-    cloud.velocityX = -3;
-    
-     //assign lifetime to the variable
-    cloud.lifetime = 200;
-    
-    //adjust the depth
-    cloud.depth = trex.depth;
-    trex.depth = trex.depth + 1;
-    
-    //add each cloud to the group
-    cloudsGroup.add(cloud);
-  }
-  
-}
-
-function spawnObstacles() {
-  if(frameCount % 60 === 0) {
-    var obstacle = createSprite(600,165,10,40);
-    obstacle.velocityX = -4;
-    
-    //generate random obstacles
-    var rand = Math.round(random(1,6));
-    switch(rand) {
-      case 1: obstacle.addImage(obstacle1);
-              break;
-      case 2: obstacle.addImage(obstacle2);
-              break;
-      case 3: obstacle.addImage(obstacle3);
-              break;
-      case 4: obstacle.addImage(obstacle4);
-              break;
-      case 5: obstacle.addImage(obstacle5);
-              break;
-      case 6: obstacle.addImage(obstacle6);
-              break;
-      default: break;
-    }
-    
-    //assign scale and lifetime to the obstacle           
-    obstacle.scale = 0.5;
-    obstacle.lifetime = 300;
-    //add each obstacle to the group
-    obstaclesGroup.add(obstacle);
-  }
 }
