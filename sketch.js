7f0938f9dfc1551fb0f64a629f2af9841da2c774
Shreeyans Sharma <64155592+Shreeyans1@users.noreply.github.com>
2020-06-02 21:02:34 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,33 +1,35 @@
-var car,wall;
-var speed,weight;
+var wall,thickness;
+var bullet,speed,weight;
 function setup() {
   createCanvas(1600,400);
-  speed = random(55,90);
-  weight = random(400,1500);
-  wall = createSprite(1500,200,60,height/2);
-  wall.shapeColor = "white";
-  car = createSprite(50,200,50,50);
-  car.velocityX = speed;
-  car.shapeColor = "white";
+  speed = random(50,55);
+  weight = random(30,52);
+  thickness = random(22,83);
+  wall = createSprite(1200,200,thickness,height/2);
+  wall.shapeColor = color(80,80,80);
+  bullet = createSprite(50,200,30,10);
+  bullet.velocityX = speed;
+  bullet.shapeColor = "white";
+  
 }
 
 function draw() {
   background(0);  
-  if(car.x-wall.x<wall.width/2+car.width/2
-    &&wall.x-car.x<wall.width/2+car.width/2
-    &&car.y-wall.y<wall.height/2+car.height/2
-    &&wall.y-car.y<wall.height/2+car.height/2){
-  car.velocityX = 0;
-  var deformation = 0.5*weight*speed*speed/22500;
-  }
-  if (deformation<100){
-    car.shapeColor = color(0,255,0);
-  }
-  if (deformation>100&&deformation<180){
-    car.shapeColor = color(230,230,0);
-  }
-  if (deformation>180){
-    car.shapeColor = color(255,0,0);
+  if(bullet.x-wall.x<wall.width/2+bullet.width/2
+    &&wall.x-bullet.x<wall.width/2+bullet.width/2
+    &&bullet.y-wall.y<wall.height/2+bullet.height/2
+    &&wall.y-bullet.y<wall.height/2+bullet.height/2){
+  bullet.velocityX = 0;
+  var damage = 0.5*weight*speed*speed/thickness*thickness*thickness;
+  
+
+   if (damage<=10){
+     wall.shapeColor = color(0,255,0);
+   }
+  
+   if (damage>10){
+     wall.shapeColor = color(255,0,0);
+   }
   }
   drawSprites();
 }
