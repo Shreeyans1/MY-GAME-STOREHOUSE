80c66dde6acef4136311293e2d62cddc327fb230
Shreeyans Sharma <64155592+Shreeyans1@users.noreply.github.com>
2020-06-03 19:15:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,35 +1,65 @@
-var wall,thickness;
-var bullet,speed,weight;
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
 function setup() {
-  createCanvas(1600,400);
-  speed = random(50,55);
-  weight = random(30,52);
-  thickness = random(22,83);
-  wall = createSprite(1200,200,thickness,height/2);
-  wall.shapeColor = color(80,80,80);
-  bullet = createSprite(50,200,30,10);
-  bullet.velocityX = speed;
-  bullet.shapeColor = "white";
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.6, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
   
 }
+
 
 function draw() {
-  background(0);  
-  if(bullet.x-wall.x<wall.width/2+bullet.width/2
-    &&wall.x-bullet.x<wall.width/2+bullet.width/2
-    &&bullet.y-wall.y<wall.height/2+bullet.height/2
-    &&wall.y-bullet.y<wall.height/2+bullet.height/2){
-  bullet.velocityX = 0;
-  var damage = 0.5*weight*speed*speed/thickness*thickness*thickness;
-  
-
-   if (damage<=10){
-     wall.shapeColor = color(0,255,0);
-   }
-  
-   if (damage>10){
-     wall.shapeColor = color(255,0,0);
-   }
-  }
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
   drawSprites();
+ 
 }
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody,false);
+    
+  }
+}
+
+
+
